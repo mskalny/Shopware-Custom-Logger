317b8dd281e4333aca4f561246da7e67df1d0807
Maciej Skalny <maciej.skalny@polcode.net>
2019-12-04 07:58:17 +0100

    Initial commit

---
+++ MskalnyCustomLogger.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @category  CustomLogger
+ * @package   SwagCustomLogger
+ * @author    Maciej Skalny <mjskalny@gmail.com>
+ * @copyright 2019 Copyright (c) mskalny
+ */
+
+namespace SwagCustomLogger;
+
+use Shopware\Components\Plugin;
+
+class MskalnyCustomLogger extends Plugin
+{
+}
---
+++ Services/Logger.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * @category  CustomLogger
+ * @package   SwagCustomLogger
+ * @author    Maciej Skalny <mjskalny@gmail.com>
+ * @copyright 2019 Copyright (c) mskalny
+ */
+
+namespace MskalnyCustomLogger\Services;
+
+use Monolog\Logger as MonologLogger;
+use Monolog\Handler\HandlerInterface;
+use Monolog\Handler\StreamHandler;
+
+/**
+ * Class Logger
+ *
+ * @package SwagCustomLogger\Services
+ */
+class Logger extends MonologLogger
+{
+    /**
+     * Logs directory.
+     */
+    const LOG_DIR = '/var/log/';
+
+    /**
+     * Log file extension.
+     */
+    const LOG_EXT = '.log';
+
+    /**
+     * @var string
+     */
+    protected $rootDir;
+
+    /**
+     * Logger constructor.
+     *
+     * @param string $name
+     * @param string $rootDir
+     * @param array $handlers
+     * @param array $processors
+     */
+    public function __construct(
+        string $name,
+        string $rootDir,
+        array $handlers = [],
+        array $processors = []
+    ) {
+        $this->rootDir = $rootDir;
+
+        parent::__construct($name, $handlers, $processors);
+    }
+
+    /**
+     * Pushes a handler on to the stack.
+     *
+     * @param HandlerInterface $handler
+     *
+     * @return $this
+     *
+     * @throws \Exception
+     */
+    public function pushHandler(HandlerInterface $handler): self
+    {
+        $handler = new StreamHandler(
+            $this->rootDir . self::LOG_DIR . $this->name . self::LOG_EXT,
+            MonologLogger::DEBUG
+        );
+
+        parent::pushHandler($handler);
+
+        return $this;
+    }
+}
